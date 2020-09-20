f84bf8bee44534b5d9ddcd543ac32ac7e73dd0f4
Kirill <iamiwk@yahoo.com>
2020-09-20 17:40:55 +0300

    added bot 4 testing

---
+++ echotest.py
@@ -0,0 +1,23 @@
+import telebot
+
+API_TOKEN = '<api_token>'
+
+bot = telebot.TeleBot(1303412518:AAHBYrrX0Ne4NwSqbNOvQlZMcS5BkBNtPDE)
+
+
+# Handle '/start' and '/help'
+@bot.message_handler(commands=['help', 'start'])
+def send_welcome(message):
+    bot.reply_to(message, """\
+Hi there, I am EchoBot.
+I am here to echo your kind words back to you. Just say anything nice and I'll say the exact same thing to you!\
+""")
+
+
+# Handle all other messages with content_type 'text' (content_types defaults to ['text'])
+@bot.message_handler(func=lambda message: True)
+def echo_message(message):
+    bot.reply_to(message, message.text)
+
+
+bot.polling()
